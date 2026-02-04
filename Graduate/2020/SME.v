827e46d33dab5ac4db6fe753407546b5e1d8655d
timchen1015 <tim.chen60101@gmail.com>
2026-02-04 17:21:08 +0800

    Add Graduate 2020 contest

---
+++ Graduate/2020/SME.v
@@ -0,0 +1,13 @@
+module SME(clk,reset,chardata,isstring,ispattern,valid,match,match_index);
+input clk;
+input reset;
+input [7:0] chardata;
+input isstring;
+input ispattern;
+output reg match;
+output reg [4:0] match_index;
+output reg valid;
+//=================================================
+
+
+endmodule
